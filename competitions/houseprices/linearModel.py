c70906d17e032a3afc074fba86cb61747056583c
terence chow <terence.chow.1986@gmail.com>
2017-06-01 00:40:09 +0100

    update gitignore, folder structure, linear regression model

--- competitions/houseprices/linearModel.py
+++ competitions/houseprices/linearModel.py
@@ -20,9 +20,11 @@ sys.path.append(root_dir)
 # now we can import our linear model
 from models.LinearRegression import LinearRegressionModel  # noqa: E402
 
+LEARNING_RATE = 0.001
 DATA_DIR = current_dir + '/data/'
 CHECKPOINT_DIR = current_dir + '/checkpoint/'
-PREDICTION_DIR = current_dir + '/prediction/'
+PREDICTION_DIR = current_dir + \
+    '/prediction/linear-model-lr-{}-'.format(LEARNING_RATE)
 
 CONTINUOUS_VARIABLES = [
     'LotFrontage', 'LotArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea',
@@ -84,19 +86,30 @@ CATEGORICAL_VARIABLES = {
 }
 
 
-def preprocess_data(filename):
-    df = pd.read_csv(DATA_DIR + filename)
-    y = df['SalePrice'].values.reshape((-1, 1)) if 'SalePrice' in df else None
-    features = df[CONTINUOUS_VARIABLES]
+def preprocess_data(data_df, train_df):
+    y = data_df['SalePrice'].values.reshape(
+        (-1, 1)) if 'SalePrice' in data_df else None
+    continuous = data_df[CONTINUOUS_VARIABLES]
 
-    # replace NANs with average for continuous variables
-    features = features.fillna(features.mean())
-    features = features.as_matrix()
+    # standardize based on the training data mean and stdev, not the test data
+    standardized = (continuous - train_df.mean()) / train_df.std()
+
+    # create a random standard normal data frame of the same shape
+    dfrand = pd.DataFrame(
+        data=np.random.standard_normal(size=standardized.shape),
+        columns=standardized.columns,
+        index=standardized.index
+    )
+
+    # use the random standard normal values to fill in the NaNs
+    standardized = standardized.fillna(dfrand)
+
+    features = standardized.as_matrix()
 
     # convert categorical variables to one hot tensors
     with tf.Session() as sess:
         for col, unique in CATEGORICAL_VARIABLES.items():
-            values = df[col].values
+            values = data_df[col].values
             depth = len(unique)
             unique_dict = {unique[u]: u for u in range(depth)}
             new_vals = []
@@ -110,19 +123,33 @@ def preprocess_data(filename):
     return features, y
 
 
+def get_data(train_file, test_file=None):
+    data_df = pd.read_csv(DATA_DIR + train_file)
+    split = int(len(data_df) * 0.8)
+    train_df, valid_df = data_df[:split], data_df[split:]
+
+    train_features, train_y = preprocess_data(train_df, train_df)
+
+    if test_file:
+        test_df = pd.read_csv(DATA_DIR + test_file)
+        test_features, test_y = preprocess_data(test_df, train_df)
+        return test_features, test_y, None, None
+    else:
+        valid_features, valid_y = preprocess_data(valid_df, train_df)
+        return train_features, train_y, valid_features, valid_y
+
+
 def predict():
-    features, _ = preprocess_data('test.csv')
+    features, _, _, _ = get_data('train.csv', 'test.csv')
 
     num_samples, num_features = features.shape
     X = tf.placeholder(tf.float32, name='X')
     Y = tf.placeholder(tf.float32, name='Y')
 
-    model = LinearRegressionModel(X, Y, 0.00000000001, num_features)
+    model = LinearRegressionModel(X, Y, LEARNING_RATE, num_features)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
-
-    # features, _ = preprocess_data('test.csv')
 
     with tf.Session() as sess:
         sess.run(init)
@@ -132,7 +159,7 @@ def predict():
 
         prediction = sess.run(model.prediction, feed_dict={X: features})
 
-        with open(PREDICTION_DIR + 'predictions.csv', 'w') as f:
+        with open(PREDICTION_DIR + 'best-model-predictions.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerow(['Id', 'SalePrice'])
             for i in range(len(prediction)):
@@ -140,13 +167,14 @@ def predict():
 
 
 def train():
-    features, y = preprocess_data('train.csv')
+    train_features, train_y, valid_features, valid_y = get_data('train.csv')
+    # features, y = preprocess_data('train.csv')
 
-    num_samples, num_features = features.shape
+    num_samples, num_features = train_features.shape
     X = tf.placeholder(tf.float32, name='X')
     Y = tf.placeholder(tf.float32, name='Y')
 
-    model = LinearRegressionModel(X, Y, 0.00000000001, num_features)
+    model = LinearRegressionModel(X, Y, LEARNING_RATE, num_features)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -154,21 +182,46 @@ def train():
     with tf.Session() as sess:
         sess.run(init)
 
-        ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
-        if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(sess, ckpt.model_checkpoint_path)
-        for i in range(100000):
+        # ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+        # if ckpt and ckpt.model_checkpoint_path:
+        #     saver.restore(sess, ckpt.model_checkpoint_path)
+
+        best_validation_loss = math.inf
+        improvement_threshold = 1.0
+        validation_frequency = 5000
+        num_epochs = 10000
+        epoch_increase = 5000
+        epoch = 0
+        while (epoch < num_epochs):
+            epoch = epoch + 1
             _, current_loss, global_step = sess.run(
                 [model.optimize, model.error, model.global_step],
-                feed_dict={X: features, Y: y})
+                feed_dict={X: train_features, Y: train_y})
 
-            print("Epoch {} finished. Average Loss: {}".format(
-                global_step, current_loss))
+            new_validation_loss = sess.run(model.error, feed_dict={
+                                           X: valid_features, Y: valid_y})
 
-            if i > 0 and global_step % 1000 == 0:
-                saver.save(sess, CHECKPOINT_DIR, global_step=model.global_step)
+            # check if validation is improving every few training epochs
+            if epoch % validation_frequency == 0:
+                if new_validation_loss < best_validation_loss:
+                    # if it is better by more than the threshold then keep
+                    # training
+                    if new_validation_loss < best_validation_loss * improvement_threshold \
+                            and epoch > num_epochs - epoch_increase:
+                        num_epochs = num_epochs + epoch_increase
+
+                    best_validation_loss = new_validation_loss
+
+                    # save the best model
+                    saver.save(sess, CHECKPOINT_DIR +
+                               'linear-model-lr-{}-best-model'.format(
+                                   LEARNING_RATE),
+                               global_step=model.global_step)
+
+            print("Epoch {} finished. Average Loss: {}, Validation Error: {}, Best Validation Error: {}"
+                  .format(global_step, current_loss, new_validation_loss, best_validation_loss))
 
 
 if __name__ == '__main__':
-    train()
-    # predict()
+    # train()
+    predict()
