15c87d34544261fbba55db190eb559a6b259f467
terence chow <terence.chow.1986@gmail.com>
2017-06-01 21:44:46 +0100

    remove commented code

--- models/LinearRegression.py
+++ models/LinearRegression.py
@@ -1,19 +1,5 @@
-# import functools
 import tensorflow as tf
-
 from utils import lazy_decorator
-
-# def lazy_decorator(function):
-#     attribute = '_cache_' + function.__name__
-#
-#     @property
-#     @functools.wraps(function)
-#     def decorator(self):
-#         if not hasattr(self, attribute):
-#             with tf.variable_scope(function.__name__):
-#                 setattr(self, attribute, function(self))
-#         return getattr(self, attribute)
-#     return decorator
 
 
 class LinearRegressionModel:
